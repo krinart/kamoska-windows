518a3b9320a8e907438aa1a17898bcf976c7e005
Viktor Yershov <krinart@gmail.com>
2024-10-17 22:02:19 -0700

    Improved form logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {Styles} from "./shared/styles";
 import {Style, SubStyle} from "./shared/types";
-import {FormControl} from '@angular/forms';
-// import {setTimeout} from "timers";
-
-const DEFAULT_DIM_VALUE = '';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,9 +14,9 @@ export class AppComponent {
   currentStyle?: Style;
   currentSubStyle?: SubStyle;
 
-  dimXControl = new FormControl(DEFAULT_DIM_VALUE);
-  dimYControl = new FormControl(DEFAULT_DIM_VALUE);
-  dimZControl = new FormControl(DEFAULT_DIM_VALUE);
+  dimXControl = new FormControl("", [Validators.required]);
+  dimYControl = new FormControl("", [Validators.required]);
+  dimZControl = new FormControl("");
 
   @ViewChild('scrollTo')
   scrollTo?: ElementRef;
@@ -41,13 +38,21 @@ export class AppComponent {
   subStyleClick(subStyle: SubStyle) {
     const isScroll = this.currentSubStyle === undefined;
     this.currentSubStyle = subStyle;
-    this.resetDimensions();
 
     if (isScroll) {
       setTimeout(() => {
         this.scrollTo?.nativeElement.scrollIntoView({behavior: 'smooth'});
       }, 100)
     }
+
+    this.resetDimensions();
+    console.log(subStyle.extraDimension);
+    if (subStyle.extraDimension === undefined) {
+      this.dimZControl.clearValidators();
+    } else {
+      this.dimZControl.setValidators([Validators.required]);
+    }
+    this.dimZControl.updateValueAndValidity();
   }
 
   getSubStyleClass(subStyle: SubStyle): string {
@@ -59,14 +64,36 @@ export class AppComponent {
   }
 
   resetDimensions() {
-    this.dimXControl.setValue(DEFAULT_DIM_VALUE);
-    this.dimYControl.setValue(DEFAULT_DIM_VALUE);
-    this.dimZControl.setValue(DEFAULT_DIM_VALUE);
+    // this.dimXControl.setValue("");
+    // this.dimYControl.setValue("");
+    // this.dimZControl.setValue("");
+
+    this.dimXControl.markAsUntouched();
+    this.dimYControl.markAsUntouched();
+    this.dimZControl.markAsUntouched();
+
+    this.dimXControl.markAsPristine();
+    this.dimYControl.markAsPristine();
+    this.dimZControl.markAsPristine();
+
+    this.dimXControl.updateValueAndValidity();
+    this.dimYControl.updateValueAndValidity();
+    this.dimZControl.updateValueAndValidity();
   }
 
   onNext() {
-    console.log(this.dimXControl.value);
-    console.log(this.dimYControl.value);
-    console.log(this.dimZControl.value);
+    const isFormFalid = this.dimXControl.valid && this.dimYControl.valid && this.dimZControl.valid;
+
+    if (isFormFalid) {
+      alert('Success')
+    } else {
+      this.dimXControl.markAsTouched();
+      this.dimYControl.markAsTouched();
+      this.dimZControl.markAsTouched();
+    }
+  }
+
+  isRequiredError(formControl: FormControl) {
+    return formControl!.invalid && (formControl!.dirty || formControl!.touched) && formControl?.hasError('required');
   }
 }
