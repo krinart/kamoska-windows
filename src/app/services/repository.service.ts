25f3079c68e24bd1bd04a4a57398e8483af1e05d
Viktor Yershov <krinart@gmail.com>
2024-10-19 17:38:46 -0700

    Update tax

--- src/app/services/repository.service.ts
+++ src/app/services/repository.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Color, Quote, QuoteItem, Style, SubStyle} from "../shared/types";
 
-const DEFAULT_SALES_TAX = 0.1;
+const DEFAULT_SALES_TAX = 10;
 
 @Injectable({
   providedIn: 'root'
@@ -26,7 +26,7 @@ export class RepositoryService {
   private updateQuoteTotal(quote: Quote) {
     const itemsTotal = quote.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
     const subtotal = itemsTotal - quote.discount;
-    const tax = (subtotal * quote.tax)
+    const tax = (subtotal * quote.tax) / 100;
     const total = subtotal + tax;
 
     quote.subtotal = Number(itemsTotal.toFixed(2));
